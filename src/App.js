4d82b4ad30c89fb2fa9def9c8751b53470c9c005
TRiley1 <thomas.edward.riley@gmail.com>
2023-06-02 19:00:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [courses, setCourses] = useState([]);
+
+useEffect(() => {
+    fetch('http://localhost:8080/courses')
+    .then(res => res.json())
+    .then(courses => setCourses(courses))
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <>
+    {courses.length}
+    </>
+  )
 }
 
 export default App;
